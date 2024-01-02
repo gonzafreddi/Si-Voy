8bba03bdd1199ddf53f084723665456a41c92e27
gonzalo freddi <freddigonzalo17@gmail.com>
2024-01-02 20:37:06 -0300

    install thunk

--- Client/src/redux/reducer/reducer.js
+++ Client/src/redux/reducer/reducer.js
@@ -1,1 +1,21 @@
- 
+ // reducer.js
+const initialState = {
+    // Define el estado inicial aquí
+  };
+  
+  const rootReducer = (state = initialState, action) => {
+    // Maneja las acciones y actualiza el estado
+    switch (action.type) {
+      // Agrega casos para manejar acciones específicas
+      // Ejemplo: 
+      // case 'INCREMENT':
+      //   return { ...state, count: state.count + 1 };
+      // case 'DECREMENT':
+      //   return { ...state, count: state.count - 1 };
+      default:
+        return state;
+    }
+  };
+  
+  export default rootReducer;
+  
