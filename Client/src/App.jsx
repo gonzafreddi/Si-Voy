68ff9fad65b1812632abe71cad3aec794ee2717d
gonzalo freddi <freddigonzalo17@gmail.com>
2024-01-04 18:32:13 -0300

    nav style

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import { Route, Routes , BrowserRouter} from 'react-router-dom';
 import Destination from './pages/destinos/Destination';
-
+import  Nav  from './components/Navbar/Nav';
 function App() {
   return (
     <>
 
     <BrowserRouter>
+    <Nav/>
     <Routes>
         <Route path='/destination' element={<Destination />} />
       </Routes>
--- Client/src/components/title/title.jsx
+++ Client/src/components/title/title.jsx
@@ -1,5 +1,5 @@
 export default function TitleSiVoy(){
-    return(<div className="absolute z-50 w-screen flex justify-center items-center h-40vh">
+    return(<div className="absolute z-50 w-screen flex justify-center items-center h-40vh top-99">
         <div className="w-3/4">
         <img className="w-12" src="https://cdn-icons-png.flaticon.com/256/9985/9985721.png" alt="" />
         <h1 className="text-gray-50 font-extrabold text-6xl">Si, Voy</h1>
