a4d26de5c58efe65785952c0f1b9164620f5a6eb
Emanuel Ivan Robledo <emanuel.robledo@hotmail.com>
2024-01-15 12:47:47 -0300

    peticiones get

--- Client/src/redux/actions/action.js
+++ Client/src/redux/actions/action.js
@@ -1,1 +1,39 @@
-  
+import axios from 'axios'
+
+export const LOADER = "LOADER"
+export const GET_BALNEARIOS = "GET_BALNEARIOS"
+export const GET_GASTRONOMIA = "GET_GASTRONOMIA"
+export const GET_PLACES = "GET_PLACES"
+
+export const getBalnearios = () => {
+    return async function (dispatch) {
+        try {
+        const {data} = await axios.get("")
+        dispatch({type: GET_BALNEARIOS, payload: data})
+    } catch (error) {
+        alert("No se encontraron balnearios");
+    }
+  }
+}
+
+export const getGastronomia = () => {
+    return async function (dispatch) {
+        try {
+        const {data} = await axios.get("")
+        dispatch({type: GET_GASTRONOMIA, payload: data})
+    } catch (error) {
+        alert("No se encontraron resultados");
+    }
+  }
+}
+
+export const getPlaces = () => {
+    return async function (dispatch) {
+        try {
+        const {data} = await axios.get("")
+        dispatch({type: GET_PLACES, payload: data})
+    } catch (error) {
+        alert("No se encontraron lugares");
+    }
+  }
+}
--- Client/src/redux/reducer/reducer.js
+++ Client/src/redux/reducer/reducer.js
@@ -1,11 +1,26 @@
  // reducer.js
 const initialState = {
     // Define el estado inicial aquí
-  };
+    GET_PLACES,
+    GET_GASTRONOMIA,
+    GET_BALNEARIOS,
+  }; from './actions'
   
+  const initialState = {
+    places: [],
+    gastronomia: [],
+    balnearios: [],
+    loader: true
+}
   const rootReducer = (state = initialState, action) => {
     // Maneja las acciones y actualiza el estado
     switch (action.type) {
+      case GET_PLACES:
+            return {...state, places: action.payload}
+      case GET_GASTRONOMIA:
+            return {...state, gastronomia: action.payload}
+      case GET_BALNEARIOS:
+            return {...state, balnearios: action.payload}
       // Agrega casos para manejar acciones específicas
       // Ejemplo: 
       // case 'INCREMENT':
@@ -15,6 +30,7 @@ const initialState = {
       default:
         return state;
     }
+  export default rootReducer;
   };
   
   export default rootReducer;
